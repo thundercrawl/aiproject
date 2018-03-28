4b3667259ff2cbf686967828a423a6ac2b85d520
unknown <JINJUN SU@PROD.HCLPNP.COM>
2018-03-28 19:34:58 +0800

    init

---
+++ k-method.py
@@ -0,0 +1,36 @@
+import os
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+class Utility:
+    def __init__(self):
+        print("init uitility class")
+    def readKnnFileToMatrix(self):
+        
+def classify0(
+    inX,dataSet,labels,k):
+    print("knn");
+    
+def drawResult():
+    print("drawresult");
+    fig=plt.figure()
+    ax=fig.add_subplot(111
+    )
+    ax.scatter(random.rand(4,4),random.rand(4,4))
+    #plt.show()
+    plt.savefig("c:\\test.png")
+
+def draw1():
+    x = np.arange(0, 5, 0.1)
+    y = np.sin(x)
+    plt.plot(x, y)
+    #plt.show()
+    plt.savefig("1.png",dpi=100)
+    plt.show()
+
+def main():
+    draw1()
+    pass
+    
+if __name__ == '__main__':
+    main()
